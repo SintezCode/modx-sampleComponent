38cd5bf9637149617779053499c544313b2dfd23
SintezCode <unarmedf@gmail.com>
2020-04-08 15:46:04 +0300

    Add snippets data

---
+++ _build/data/transport.snippets.php
@@ -0,0 +1,28 @@
+<?php
+
+$sconfig=[
+    'sampleSnippet'=>[
+        'description'=>'SampleComponent snippet',
+    ],
+];
+
+foreach($sconfig?:[] as $snippet=>$options){
+    $snippet_file=$config['component']['core'].'elements/snippets/'.$snippet.'.php';
+    if(!file_exists($snippet_file))continue;
+    $data['modSnippet'][$snippet]=[
+        'fields'=>[
+            'name' => $snippet,
+            'description' => $options['description'],
+            'snippet' => trim(str_replace(['<?php', '?>'], '', file_get_contents($snippet_file))),
+            'source' => 1,
+        ],
+        'options'=>[
+            'search_by'=>['name'],
+        ],
+        'relations'=>[
+            'modCategory'=>[
+                'main'=>'Snippets'
+            ]
+        ]
+    ];
+}
